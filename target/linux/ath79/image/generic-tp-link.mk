d4e1ac0732d7c13a251f463af6fc7d61fe4695e9
Lech Perczak <lech.perczak@gmail.com>
2019-12-01 19:31:32 +0100

    ath79: add support for TP-Link TL-WR902AC v1
    TP-Link TL-WR902AC v1 is a pocket-size, dual-band (AC750), successor of
    TL-MR3020 (both devices use very similar enclosure, in same size). New
    device is based on Qualcomm QCA9531 v2 + QCA9887. FCC ID: TE7WR902AC.
    
    Specification:
    
    - 650/391/216 MHz (CPU/DDR/AHB)
    - 1x 10/100 Mbps Ethernet
    - 1x USB 2.0 (GPIO-controlled power)
    - 64 MB of RAM (DDR2)
    - 8 MB of FLASH
    - 2T2R 2.4 GHz (QCA9531)
    - 1T1R 5 GHz (QCA9887)
    - 5x LED (GPIO-controlled), 2x button, 1x 3-pos switch
    - UART pads on PCB (TP1 -> TX, TP2 -> RX, TP3 -> GND, TP4 -> 3V3, jumper
      resitors are missing on TX/RX lines)
    - 1x micro USB (for power only)
    
    Flash instructions:
    
    Use "factory" image under vendor GUI.
    
    Recovery instructions:
    
    This device contains tftp recovery mode inside U-Boot. You can use it to
    flash OpenWrt (use "factory" image) or vendor firmware.
    
    1. Configure PC with static IP 192.168.0.66/24 and tftp server.
    2. Rename "openwrt-ath79-generic-tplink_tl-wr902ac-v1-squashfs-factory.bin"
       to "wr902acv1_un_tp_recovery.bin" and place it in tftp server dir.
    3. Connect PC with LAN port, press the reset button, power up the router
       and keep button pressed until WPS LED lights up.
    4. Router will download file from server, write it to flash and reboot.
    
    Root access over serial line in vendor firmware: root/sohoadmin.
    
    Based on support in ar711x target by: Piotr Dymacz <pepe2k@gmail.com>
    
    Signed-off-by: Lech Perczak <lech.perczak@gmail.com>
    (cherry picked from commit a8653fc6a81681c49404fd9324939ec95fc866b5)

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -363,6 +363,20 @@ define Device/tplink_tl-wr842n-v3
 endef
 TARGET_DEVICES += tplink_tl-wr842n-v3
 
+define Device/tplink_tl-wr902ac-v1
+  $(Device/tplink-safeloader)
+  ATH_SOC := qca9531
+  DEVICE_TITLE := TP-Link TL-WR902AC v1
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport \
+    kmod-ath10k-ct-smallbuffers ath10k-firmware-qca9887-ct -swconfig -uboot-envtools
+  TPLINK_BOARD_ID := TL-WR902AC-V1
+  IMAGE_SIZE := 7360k
+  TPLINK_HWID := 0x0
+  TPLINK_HWREV := 0
+  SUPPORTED_DEVICES += tl-wr902ac-v1
+endef
+TARGET_DEVICES += tplink_tl-wr902ac-v1
+
 define Device/tplink_tl-wr1043nd-v1
   $(Device/tplink-8m)
   ATH_SOC := ar9132
