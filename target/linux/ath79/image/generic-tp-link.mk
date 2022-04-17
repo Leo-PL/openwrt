dbaf48491511deaf4fc486e02739426ed38d7a77
Lech Perczak <lech.perczak@gmail.com>
2022-04-17 22:43:22 +0200

    ath79: support TL-WR902AC v1 modded with 16M flash
    The device uses completely rewritten flash layout, based on previous
    non-safeloader layout. Requires u-boot_mod to work properly.
    MAC is moved to previous location at 1fc00 and needs to be embedded in
    U-boot manually.
    
    Signed-off-by: Lech Perczak <lech.perczak@gmail.com>

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -377,6 +377,18 @@ define Device/tplink_tl-wr902ac-v1
 endef
 TARGET_DEVICES += tplink_tl-wr902ac-v1
 
+define Device/tplink_tl-wr902ac-v1-16m
+  $(Device/tplink-16mlzma)
+  ATH_SOC := qca9531
+  DEVICE_TITLE := TP-Link TL-WR902AC v1 (16M)
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport \
+	kmod-ath10k-ct-smallbuffers ath10k-firmware-qca9887-ct \
+	-swconfig -uboot-envtools
+  TPLINK_HWID := 0x9020001
+  SUPPORTED_DEVICES += tl-wr902ac-v1 tplink,tl-wr902ac-v1
+endef
+TARGET_DEVICES += tplink_tl-wr902ac-v1-16m
+
 define Device/tplink_tl-wr1043nd-v1
   $(Device/tplink-8m)
   ATH_SOC := ar9132
